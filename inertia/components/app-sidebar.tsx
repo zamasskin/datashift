9b33a92044418cd3cffd8d9e2a2a6efb6941ed80
Alexey Zamasskin <zamasskin@yandex.ru>
2025-11-14 13:33:56 +0300

    refactor(sidebar): improve user data handling and navigation structure
    - Replace hardcoded user data with User model
    - Simplify navigation menu structure and add admin check
    - Use user's initials for avatar fallback

--- inertia/components/app-sidebar.tsx
+++ inertia/components/app-sidebar.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import {
   IconBrandCodepen,
-  IconTornado,
   IconArrowUpToArc,
-  IconBrandAsana,
   IconSettings,
   IconHelp,
   IconSearch,
@@ -18,62 +16,57 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '~/components/ui/sidebar'
-import { Link } from '@inertiajs/react'
+import { Link, usePage } from '@inertiajs/react'
 import { NavMain } from './nav-main'
 import { NavUser } from './user-naw'
 import { NavSecondary } from './nav-secondary'
 import { BrandMark } from '~/components/ui/brand-logo'
 import { useMigrationRuns } from '~/store/migrations'
 import { RunningIndicators } from './running-indicators'
+import User from '#models/user'
 
-const data = {
-  user: {
-    name: 'shadcn',
-    email: 'm@example.com',
-    avatar: 'https://github.com/shadcn.png',
+const nawMain = [
+  {
+    title: 'Источники данных',
+    url: '/sources',
+    icon: IconBrandCodepen,
   },
-  navMain: [
-    {
-      title: '1 — Источники данных',
-      url: '/sources',
-      icon: IconBrandCodepen,
-    },
-    {
-      title: '2 — Датасеты',
-      url: '/datasets',
-      icon: IconTornado,
-    },
-    {
-      title: '3 — Миграции',
-      url: '/migrations',
-      icon: IconArrowUpToArc,
-    },
-    {
-      title: '4 — Задания',
-      url: '/tasks',
-      icon: IconBrandAsana,
-    },
-  ],
-  navSecondary: [
-    {
-      title: 'Настройки',
-      url: '/settings',
-      icon: IconSettings,
-    },
-    {
-      title: 'Помощь',
-      url: '/help',
-      icon: IconHelp,
-    },
-    {
-      title: 'Поиск',
-      url: '#',
-      icon: IconSearch,
-    },
-  ],
-}
+
+  {
+    title: 'Миграции',
+    url: '/migrations',
+    icon: IconArrowUpToArc,
+  },
+]
+
+const navSecondary = [
+  {
+    title: 'Помощь',
+    url: '/help',
+    icon: IconHelp,
+  },
+  {
+    title: 'Поиск',
+    url: '#',
+    icon: IconSearch,
+  },
+]
+
+const navSecondaryAdmin = [
+  {
+    title: 'Настройки',
+    url: '/settings',
+    icon: IconSettings,
+  },
+  ...navSecondary,
+]
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const page = usePage<{ user: User }>()
+  const isAdmin = page.props?.user?.role === 'admin'
+
+  console.log(isAdmin)
+
   const { runnings } = useMigrationRuns()
   return (
     <Sidebar collapsible="offcanvas" {...props}>
@@ -94,12 +87,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
+        <NavMain items={nawMain} />
 
         {/* Индикаторы запущенных процессов */}
         <RunningIndicators runnings={runnings} />
 
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
+        <NavSecondary items={isAdmin ? navSecondaryAdmin : navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
--- inertia/components/user-naw.tsx
+++ inertia/components/user-naw.tsx
@@ -1,3 +1,4 @@
+import User from '#models/user'
 import { Link, usePage, router } from '@inertiajs/react'
 import { IconDotsVertical, IconLogout, IconUserCircle } from '@tabler/icons-react'
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
@@ -19,13 +20,15 @@ import {
 export function NavUser() {
   const avatar = 'https://github.com/shadcn.png'
   const { isMobile } = useSidebar()
-  const { props: pageProps } = usePage<any>()
-  const user = (pageProps?.user || pageProps?.auth?.user || null) as
-    | { fullName?: string; name?: string; email?: string }
-    | null
+  const { props: pageProps } = usePage<{ user: User }>()
+  const user = pageProps?.user
   const csrfToken = (pageProps?.csrfToken || '') as string
-  const fullName = user?.fullName || user?.name || ''
+  const fullName = user?.fullName || ''
   const email = user?.email || ''
+  const fnSmall = fullName
+    .split(' ')
+    .map((part) => part[0])
+    .join('')
 
   return (
     <SidebarMenu>
@@ -37,8 +40,8 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={avatar} alt={fullName} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                {user.avatarUrl && <AvatarImage src={user.avatarUrl} alt={fullName} />}
+                <AvatarFallback className="rounded-lg">{fnSmall}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{fullName || 'Гость'}</span>
@@ -56,8 +59,8 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={avatar} alt={fullName} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  {user.avatarUrl && <AvatarImage src={avatar} alt={fullName} />}
+                  <AvatarFallback className="rounded-lg">{fnSmall}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{fullName || 'Гость'}</span>
